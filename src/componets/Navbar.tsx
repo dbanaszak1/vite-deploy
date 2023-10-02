2870a97eb23591370034e9c499a779ea3c4b8937
Dominik Banaszak <domban3@st.amu.edu.pl>
2023-10-02 13:36:44 +0200

    fix: routing&width

--- src/componets/Navbar.tsx
+++ src/componets/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="sticky top-0 z-10 bg-white shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] p-4 inline-flex w-full h-20">
     <div className="text-gray-700 text-xl w-1/2 text-center items-center inline-flex">
         <div className="m-auto h-full inline-flex items-center">
-            <img src="src\images\logo-black.png" className="h-full rounded-full mx-2"></img>
+            <img src=".\src\images\logo-black.png" className="h-full rounded-full mx-2"></img>
             <span>SkyTravel</span>
         </div>
     </div>
@@ -22,13 +22,13 @@ const Navbar = () => {
 
     <ul className="space-x-4 text-gray-700 w-1/2 text-3xl place-content-center hidden lg:flex">
       <li>
-        <a href="/home" className="hover:text-gray-500">Home</a>
+        <a href="/vite-deploy/home" className="hover:text-gray-500">Home</a>
       </li>
       <li>
-        <a href="/travel" className="text-2xl hover:text-gray-500">Travel</a>
+        <a href="/vite-deploy/travel" className="text-2xl hover:text-gray-500">Travel</a>
       </li>
       <li>
-        <a href="/contact" className="text-xl hover:text-gray-500">Contact</a>
+        <a href="/vite-deploy/contact" className="text-xl hover:text-gray-500">Contact</a>
       </li>
     </ul>    
 
@@ -36,13 +36,13 @@ const Navbar = () => {
   <div className={false === currentState ? "hidden" : "flex text-3xl w-full sticky top-20 border-b-2 border-black z-10"}>
     <ul className="bg-white w-full flex-row text-center">
       <li className='w-full text-center py-2 hover:bg-gray-700 hover:text-white duration-300'>
-        <a href="/home">Home</a>
+        <a href="/vite-deploy/home">Home</a>
       </li>
       <li className="py-2 border-y-2 text-center hover:bg-gray-700 hover:text-white duration-300">
-        <a href="/travel">Travel</a>
+        <a href="/vite-deploy/travel">Travel</a>
       </li>
       <li className="py-2 text-center hover:bg-gray-700 hover:text-white duration-300">
-        <a href="/contact">Contact</a>
+        <a href="/vite-deploy/contact">Contact</a>
       </li>
     </ul>   
   </div>  
